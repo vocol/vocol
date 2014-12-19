21b34aa998556951ae554616469a03b8bd5d28ea
lavdim <lavdimhalilaj@yahoo.com>
2014-12-19 13:21:26 +0100

    - Added template.html, which is used for generation of schema.rdfa file. - Added schemasTemplate.html, which is used for generation of schemas.html file. - Added complete execution command line form including parameters, and class library. - Added command line for recursively download using wget

--- HtmlGenerator/src/HtmlGenerator.java
+++ HtmlGenerator/src/HtmlGenerator.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import org.apache.jena.iri.impl.Main;
+import org.apache.jena.riot.RiotException;
 import org.apache.jena.riot.RiotNotFoundException;
 
 import com.hp.hpl.jena.query.QueryExecution;
@@ -19,19 +20,37 @@ import com.hp.hpl.jena.util.FileManager;
 public class HtmlGenerator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		SparqlTest(args[0], args[1]);
-	}
-
-	public static void SparqlTest(String _source, String _destination) {
 
 		try {
+
+			if (args.length == 5) {
+				GenerateSchemasHTML(args[0], args[3], args[4]);
+			}
+
+			GenerateSchemaRDFa(args[0], args[1], args[2]);
+
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out
+					.println("Missing argument! 1st:SourceFile; 2nd: DestinationFile;"
+							+ "3rd: Template; 4th (Optional): schemas.html Path; 5th (Optional): schemas.html Template Path!");
+		} catch (RiotNotFoundException e) {
+			System.out.println("File not found.Please verify file location!");
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found.Please verify file location!");
+		} catch (RiotException e) {
+			System.out
+					.println(e.getMessage() + " \n on file: " + args[0] + "!");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void GenerateSchemaRDFa(String _source, String _destination,
+			String _template) throws IOException {
+		
 			FileManager.get()
 					.addLocatorClassLoader(Main.class.getClassLoader());
-
-			// Model model =
-			// FileManager.get().loadModel("/home/lavdim/Downloads/JavaExample/DataFiles/ChargingPoints.ttl");
 
 			Model model = FileManager.get().loadModel(_source);
 
@@ -48,145 +67,239 @@ public class HtmlGenerator {
 					+ "PREFIX vcard:		        <http://www.w3.org/2001/vcard-rdf/3.0#> "
 					+ "PREFIX skos: 			<http://www.w3.org/2004/02/skos/core#> "
 					+ "PREFIX dbr: 			<http://dbpedia.org/resource/> "
-					+ "SELECT ?subject ?o WHERE { ?subject rdf:type ?o }";
-
-			// Query query = QueryFactory.create(queryString);
+					+ "SELECT DISTINCT ?subject ?o WHERE { ?subject a ?o. FILTER(?o not in(owl:Class, owl:ObjectProperty, owl:DatatypeProperty ))}"
+					+ "";
 
 			QueryExecution qexec = QueryExecutionFactory.create(queryString,
 					model);
 
-			BufferedReader in = new BufferedReader(
-					new FileReader(
-							"/home/lavdim/Downloads/JavaExample/DataFiles/template.html"));
-
+			BufferedReader in;
 			String strLine, htmlString = "", body = "";
+
+			in = new BufferedReader(new FileReader(_template));
 			while ((strLine = in.readLine()) != null) {
 				htmlString += strLine + "\n";
 			}
+				ResultSet result = qexec.execSelect(), resultProperties;
 
-			ResultSet result = qexec.execSelect(), resultProperties;
+				String resourceName = "";
 
-			String resourceName = "";
+				while (result.hasNext()) {
 
-			while (result.hasNext()) {
+					QuerySolution soln = result.nextSolution();
 
-				QuerySolution soln = result.nextSolution();
+					RDFNode node;
 
-				RDFNode node;
+					/*
+					 * node = soln.get("subject"); body +=
+					 * "<div typeof=\"rdfs:Class\" resource=\"http://schema.org/"
+					 * + node.asResource().getLocalName()+ "\">\n";
+					 */
 
-				String resourceType = soln.get("o").asResource().toString();
-
-				if (resourceType.contains("Class")) {
+					String resourceType = soln.get("o").asResource().toString();
 					node = soln.get("subject");
-					body += "<div typeof=\"rdfs:Class\" resource=\""
-							+ node.asResource().getURI() + "\">\n";
 
-				} else {
-					node = soln.get("subject");
-					body += "<div typeof=\"rdfs:Property\" resource=\""
-							+ node.asResource().getURI() + "\">\n";
+					if (resourceType.contains("Class")) {
 
-				}
+						body += "<div typeof=\"rdfs:Class\" resource=\"http://schema.org/"
+								+ node.asResource().getLocalName() + "\">\n";
 
-				queryString = "PREFIX mv: <http://purl.org/net/mobivoc/> "
-						+ "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> "
-						+ "PREFIX owl: <http://www.w3.org/2002/07/owl#> "
-						+ "PREFIX rdf:    		<http://www.w3.org/1999/02/22-rdf-syntax-ns#> "
-						+ "PREFIX xsd:     		<http://www.w3.org/2001/XMLSchema#> "
-						+ "PREFIX voaf:    		<http://purl.org/vocommons/voaf#> "
-						+ "PREFIX dct:     		<http://purl.org/dc/terms/> "
-						+ "PREFIX foaf:    		<http://xmlns.com/foaf/0.1/> "
-						+ "PREFIX s:    			<http://schema.org/> "
-						+ "PREFIX gr:    			<http://purl.org/goodrelations/v1#> "
-						+ "PREFIX vcard:		        <http://www.w3.org/2001/vcard-rdf/3.0#> "
-						+ "PREFIX skos: 			<http://www.w3.org/2004/02/skos/core#> "
-						+ "PREFIX dbr: 			<http://dbpedia.org/resource/> "
-						+ "SELECT ?s ?pr ?o ?language ?pureText WHERE { mv:"
-						+ node.asResource().getLocalName()
-						+ " ?pr ?o .  FILTER(?pr in(rdfs:label,rdfs:comment,rdfs:domain,rdfs:range,rdfs:subPropertyOf,rdfs:subClassOf)) "
-						+ "   BIND (lang(?o) AS ?language)  BIND (str(?o) AS ?pureText) }";
+					} else {
 
-				qexec = QueryExecutionFactory.create(queryString, model);
-				resultProperties = qexec.execSelect();
+						body += "<div typeof=\"rdf:Property\" resource=\"http://schema.org/"
+								+ node.asResource().getLocalName() + "\">\n";
 
-				while (resultProperties.hasNext()) {
-
-					soln = resultProperties.nextSolution();
-
-					resourceName = soln.get("pr").toString();
-
-					if (resourceName.contains("label")) {
-						body += "<span class=\"h\" property=\"rdfs:label\" lang=\""
-								+ soln.get("language").toString() + "\">";
-						resourceName = soln.get("pureText").toString();
-						body += resourceName + "</span>\n";
-					} else if (resourceName.contains("comment")) {
-						body += "<span property=\"rdfs:comment\">";
-						resourceName = soln.get("o").toString();
-						body += resourceName + "</span>\n";
-					} else if (resourceName.contains("domain")) {
-						node = soln.get("o");
-						body += "<span> Domain: <a property=\"http://schema.org/domainIncludes\"";
-						resourceName = " href=\"" + soln.get("o").toString()
-								+ "\">" + node.asResource().getLocalName()
-								+ "</a>";
-						body += resourceName + "</span>\n";
-					} else if (resourceName.contains("range")) {
-						node = soln.get("o");
-						body += "<span> Range: <a property=\"http://schema.org/rangeIncludes\"";
-						resourceName = " href=\"" + soln.get("o").toString()
-								+ "\">" + node.asResource().getLocalName()
-								+ "</a>";
-						body += resourceName + "</span>\n";
-					} else if (resourceName.contains("subClassOf")) {
-						node = soln.get("o");
-						body += "<span> Subclass of: <a property=\"rdfs:subClassOf\"";
-						resourceName = " href=\"" + soln.get("o").toString()
-								+ "\">" + node.asResource().getLocalName()
-								+ "</a>";
-						body += resourceName + "</span>\n";
-					} else if (resourceName.contains("subPropertyOf")) {
-						node = soln.get("o");
-						body += "<link property=\"rdfs:subPropertyOf\"";
-						resourceName = " href=\"" + soln.get("o").toString()
-								+ "\" />";
-						body += resourceName;
 					}
 
-					// resourceName = soln.get("o").toString();
-					// body += resourceName + "</span>\n";
+					queryString = "PREFIX mv: <http://purl.org/net/mobivoc/> "
+							+ "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> "
+							+ "PREFIX owl: <http://www.w3.org/2002/07/owl#> "
+							+ "PREFIX rdf:    		<http://www.w3.org/1999/02/22-rdf-syntax-ns#> "
+							+ "PREFIX xsd:     		<http://www.w3.org/2001/XMLSchema#> "
+							+ "PREFIX voaf:    		<http://purl.org/vocommons/voaf#> "
+							+ "PREFIX dct:     		<http://purl.org/dc/terms/> "
+							+ "PREFIX foaf:    		<http://xmlns.com/foaf/0.1/> "
+							+ "PREFIX s:    			<http://schema.org/> "
+							+ "PREFIX gr:    			<http://purl.org/goodrelations/v1#> "
+							+ "PREFIX vcard:		        <http://www.w3.org/2001/vcard-rdf/3.0#> "
+							+ "PREFIX skos: 			<http://www.w3.org/2004/02/skos/core#> "
+							+ "PREFIX dbr: 			<http://dbpedia.org/resource/> "
+							+ "SELECT ?s ?pr ?o ?language ?pureText WHERE { mv:"
+							+ node.asResource().getLocalName()
+							+ " ?pr ?o . "
+							+ " "
+							+ " FILTER(?pr in(rdfs:label,rdfs:comment,rdfs:domain,rdfs:range,rdfs:subPropertyOf,rdfs:subClassOf)) "
+							+ "   BIND (lang(?o) AS ?language)  BIND (str(?o) AS ?pureText) "
+							+ " }";
 
+					qexec = QueryExecutionFactory.create(queryString, model);
+					resultProperties = qexec.execSelect();
+
+					while (resultProperties.hasNext()) {
+
+						soln = resultProperties.nextSolution();
+
+						resourceName = soln.get("pr").toString();
+
+						if (resourceName.contains("label")) {
+							// body +=
+							// "<span class=\"h\" property=\"rdfs:label\" >";
+							body += "<span class=\"h\" property=\"rdfs:label\" lang=\""
+									+ soln.get("language").toString() + "\">";
+							resourceName = soln.get("pureText").toString();
+							body += resourceName + "</span>\n";
+						} else if (resourceName.contains("comment")) {
+							body += "<span property=\"rdfs:comment\">";
+							resourceName = soln.get("o").toString();
+							body += resourceName + "</span>\n";
+						} else if (resourceName.contains("domain")) {
+							node = soln.get("o");
+							body += "<span> Domain: <a property=\"http://schema.org/domainIncludes\"";
+							resourceName = " href=\"http://schema.org/"
+									+ soln.get("o").asResource().getLocalName()
+									+ "\">" + node.asResource().getLocalName()
+									+ "</a>";
+							body += resourceName + "</span>\n";
+						} else if (resourceName.contains("range")) {
+							node = soln.get("o");
+							body += "<span> Range: <a property=\"http://schema.org/rangeIncludes\"";
+							resourceName = " href=\"http://schema.org/"
+									+ soln.get("o").asResource().getLocalName()
+									+ "\">" + node.asResource().getLocalName()
+									+ "</a>";
+							body += resourceName + "</span>\n";
+						} else if (resourceName.contains("subClassOf")) {
+							node = soln.get("o");
+							body += "<span> Subclass of: <a property=\"rdfs:subClassOf\"";
+							resourceName = " href=\"http://schema.org/"
+									+ soln.get("o").asResource().getLocalName()
+									+ "\">" + node.asResource().getLocalName()
+									+ "</a>";
+							body += resourceName + "</span>\n";
+						} else if (resourceName.contains("subPropertyOf")) {
+							node = soln.get("o");
+							body += "<link property=\"rdfs:subPropertyOf\"";
+							resourceName = " href=\"http://schema.org/"
+									+ soln.get("o").asResource().getLocalName()
+									+ "\" />";
+							// resourceName = " href=\"" +
+							// soln.get("o").toString()
+							// + "\" />";
+							body += resourceName;
+						}
+
+					}
+
+					body += "</div>";
+					body += "\n\n";
 				}
 
-				body += "</div>";
-				body += "\n\n";
-			}
+				htmlString = htmlString.replace("$divs", body);
 
-			htmlString = htmlString.replace("$divs", body);
+				PrintWriter outputStream = null;
 
-			PrintWriter outputStream = null;
+				// outputStream = new PrintWriter(new
+				// FileOutputStream("/home/lavdim/Downloads/schema-org/schemaorg/data/schema.rdfa"));
+				outputStream = new PrintWriter(new FileOutputStream(
+						_destination));
 
-			// outputStream = new PrintWriter(new
-			// FileOutputStream("/home/lavdim/Downloads/schema-org/schemaorg/data/schema.rdfa"));
-			outputStream = new PrintWriter(new FileOutputStream(_destination));
+				int count;
+				for (count = 1; count <= 1; count++) {
+					outputStream.println(htmlString);
+				}
+				outputStream.close();
+				System.out.println("File has been generated succesfully.");
+	
 
-			int count;
-			for (count = 1; count <= 1; count++) {
-				outputStream.println(htmlString);
-			}
-			outputStream.close();
+	}
 
-		} catch (FileNotFoundException e) {
-			System.out.println("File not found.Please verify location!");
-		} 
-	 catch (RiotNotFoundException e) {
-		System.out.println("File not found.Please verify location!");
-	} catch (IOException e) {
+	public static void GenerateSchemasHTML(String _source, String _destination, String _template) throws IOException {
 
-			System.out.println("Other Exception!");
+		FileManager.get().addLocatorClassLoader(Main.class.getClassLoader());
+
+		Model model = FileManager.get().loadModel(_source);
+
+		String queryString = "PREFIX mv: <http://purl.org/net/mobivoc/> "
+				+ "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> "
+				+ "PREFIX owl: <http://www.w3.org/2002/07/owl#> "
+				+ "PREFIX rdf:    		<http://www.w3.org/1999/02/22-rdf-syntax-ns#> "
+				+ "PREFIX xsd:     		<http://www.w3.org/2001/XMLSchema#> "
+				+ "PREFIX voaf:    		<http://purl.org/vocommons/voaf#> "
+				+ "PREFIX dct:     		<http://purl.org/dc/terms/> "
+				+ "PREFIX foaf:    		<http://xmlns.com/foaf/0.1/> "
+				+ "PREFIX s:    			<http://schema.org/> "
+				+ "PREFIX gr:    			<http://purl.org/goodrelations/v1#> "
+				+ "PREFIX vcard:		        <http://www.w3.org/2001/vcard-rdf/3.0#> "
+				+ "PREFIX skos: 			<http://www.w3.org/2004/02/skos/core#> "
+				+ "PREFIX dbr: 			<http://dbpedia.org/resource/> "
+				+ "SELECT DISTINCT ?subject ?o WHERE { ?subject a ?o. FILTER(?o not in(owl:Class, owl:ObjectProperty, owl:DatatypeProperty ))}"
+				+ "  ORDER BY DESC(?o)";
+
+		QueryExecution qexec = QueryExecutionFactory.create(queryString, model);
+
+		BufferedReader in = new BufferedReader(new FileReader(_template));
+
+		String strLine, htmlString = "", body = "";
+		while ((strLine = in.readLine()) != null) {
+			htmlString += strLine + "\n";
 		}
+
+		ResultSet result = qexec.execSelect(), resultProperties;
+
+		String resourceName = "", entityType="";
+
+		while (result.hasNext()) {
+
+			QuerySolution soln = result.nextSolution();
+
+			RDFNode node = soln.get("subject");
+			
+			String resourceType = soln.get("o").asResource().toString();
+			
+			if (resourceType.contains("Class")) {
+
+				if(entityType.equals("") || !entityType.equals("Class"))
+				{
+					entityType="Class";
+					body += "<br><b>Classes</b>"; 
+				}
+
+			} else if (resourceType.contains("Property"))  {
 		
-		
+				if(entityType.equals("") || !entityType.equals("Property"))
+				{
+					entityType="Property";
+					body += "<br><b>Properties</b>"; 
+				}
+
+			} else {
+				
+				if(entityType.equals("") || !entityType.equals("Intance"))
+				{
+					entityType="Intance";
+					body += "<br><b>Instances</b>"; 
+				}
+
+			}
+
+			body += "<li><a href=\"../" + node.asResource().getLocalName()
+					+ "\">" + node.asResource().getLocalName() + "</a></li>\n";
+
+		}
+
+		htmlString = htmlString.replace("$li", body);
+
+		PrintWriter outputStream = null;
+
+		outputStream = new PrintWriter(new FileOutputStream(_destination));
+
+		int count;
+		for (count = 1; count <= 1; count++) {
+			outputStream.println(htmlString);
+		}
+		outputStream.close();
+		System.out.println("SchemasHTML has been generated succesfully.");
+
 	}
 
 }
